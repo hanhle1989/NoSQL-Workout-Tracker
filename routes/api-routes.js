748cf1c30209efbdd95b2654b3565c23009129c6
Hanh Le <hanhdtle@gmail.com>
2021-04-22 17:43:41 -1000

    fixed api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,44 +1,48 @@
-module.exports = function (app) {
-    const Workout = require("../models/workouts");
+const Workout = require("../models/workouts");
 
+module.exports = function (app) {
     app.get("/api/workouts", (req, res) => {
         Workout.find({})
-            .then(data => {
-                res.send(data);
-            })
-            .catch(err => {
-                res.json(err);
-            });
-    });
-
-    app.put("/api/workouts/:id", ({ body, params }, res) => {
-        Workout.findByIdAndUpdate(params.id, { $push: { exercise: body } }, { new: true, runValidators: true })
-            .then(dbWorkout => {
-                res.json(dbWorkout);
-            })
-            .catch(err => {
-                res.json(err);
-            });
-    });
-
-    app.post("/api/workouts", (req, res) => {
-        Workout.create({})
-            .then(dbWorkout => {
-                res.json(dbWorkout);
-            })
-            .catch(err => {
-                res.json(err);
-            });
-    });
-
-    app.post("/api/workouts/range", (req, res) => {
-        Workout.create({})
             .then(data => {
                 res.json(data);
             })
             .catch(err => {
                 res.json(err);
             });
+    });
+
+    app.post("/api/workouts", async (req, res) => {
+        try {
+            const response = await Workout.create({ type: "workout" })
+            res.json(response);
+        }
+        catch (err) {
+            console.log("error occurred while trying to create a workout: ", err)
+        }
+    });
+
+    app.put("/api/workouts/:id", ({ body, params }, res) => {
+        const workoutId = params.id;
+        let savedExercises = [];
+        Workout.find({ _id: workoutId })
+            .then(dbWorkout => {
+                savedExercises = dbWorkout[0].exercises;
+                res.json(dbWorkout[0].exercises);
+                let allExercises = [...savedExercises, body]
+                console.log(allExercises)
+                updateWorkout(allExercises)
+            })
+            .catch(err => {
+                res.json(err);
+            });
+
+        function updateWorkout(exercises) {
+            Workout.findByIdAndUpdate(workoutId, { exercises: exercises }, function (err, doc) {
+                if (err) {
+                    console.log(err)
+                }
+            })
+        }
     });
 
     app.get("/api/workouts/range", (req, res) => {
