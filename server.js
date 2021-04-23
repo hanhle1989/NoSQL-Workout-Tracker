12028db78b12cd665c0653e1d588b070d177d302
Hanh Le <hanhdtle@gmail.com>
2021-04-22 19:05:10 -1000

    heroku push attempt

--- server.js
+++ server.js
@@ -12,7 +12,14 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {useNewUrlParser: true, useFindAndModify: false });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
 
 require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
